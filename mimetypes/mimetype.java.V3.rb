148b3d64fd496627274b0fc96edb7dd409c31661
itarc <itarc@numericable.fr>
2014-05-19 15:41:30 +0200

    mime type update

--- mimetypes/mimetype.java.V3.rb
+++ mimetypes/mimetype.java.V3.rb
@@ -1,3 +1,21 @@
+# Lecture des entrées
+
+#~ N = STDIN.gets.to_i
+#~ Q = STDIN.gets.to_i
+
+#~ mime_type_of = {}
+#~ N.times do
+  #~ extension, mime_type = STDIN.gets.split
+  #~ mime_type_of[extension.downcase] = mime_type
+#~ end
+#~ #p mime_type_of
+
+#~ files = []
+#~ Q.times do
+  #~ files << STDIN.gets.strip
+#~ end
+#~ #p files
+
 # init
 def mime_types(fichiers, table)
 
@@ -10,9 +28,9 @@ for i in 0..fichiers.size-1 do
     else
 	ext = nomFichier[nomFichier.rindex(".")+1..-1].downcase
 	if table.has_key?(ext)
-	    response.push(table[ext])
+response.push(table[ext])
 	else
-	    response.push("UNKNOWN")
+response.push("UNKNOWN")
 	end
     end
 end
@@ -257,88 +275,190 @@ def mime_types(file_names, mime_type_of)
   file_names.map { |file_name|  mime_type_of[ext(file_name).downcase] || "UNKNOWN" }
 end
 
+# moving downcase in ext function
+def ext(file_name)
+  (File.extname(file_name)[1..-1] || "").downcase
+end
+
+def mime_types(file_names, mime_type_of)
+  file_names.map { |file_name|  mime_type_of[ext(file_name)] || "UNKNOWN" }
+end
+
+
+## ----------------------------------------------------
+## ----------- JAVA VERSION (BEGIN)----
+## ----------------------------------------------------
+
+class Solution
+
+  def Solution.main(table, fichiers)
+    types_mimes = Array.new();
+    for i in 0..fichiers.size-1 do
+      nomFichier = fichiers[i];
+      if (nomFichier.rindex(".") == nil)
+        types_mimes.push("UNKNOWN");
+      else
+        ext = nomFichier[nomFichier.rindex(".")+1, 
+	                           nomFichier.size-1];
+        ext = ext.downcase;
+        if (table.has_key?(ext))
+          types_mimes.push(table[ext]);
+        else
+          types_mimes.push("UNKNOWN");
+        end
+      end
+      i = i + 1;
+    end
+    return types_mimes;
+  end
+	
+end
+
+def mime_types(table, fichiers)
+  Solution.main(table, fichiers);
+end
+
+
+## ----------------------------------------------------
+## ----------- JAVA VERSION (END)----
+## ----------------------------------------------------
+
+## ----------------------------------------------------
+## ----------- RUBY VERSION (END)----
+## ----------------------------------------------------
+def ext(file_name)
+  (File.extname(file_name)[1..-1] || "").downcase
+end
+
+def mime_types(mime_type_of, file_names)
+  file_names.map { |file_name|  mime_type_of[ext(file_name)] || 'UNKNOWN' }
+end
+## ----------------------------------------------------
+## ----------- RUBY VERSION (END)----
+## ----------------------------------------------------
+
+
+
 require 'test/unit'
 
-class TestsSession < Test::Unit::TestCase
+class TestMimeType < Test::Unit::TestCase
 
-  def test03
-    extension_hash = { "html" => "text/html" }
-    file_list = ["file.html"]
+  def test01_empty_list
+
+    extension_hash = { 
+    }
+    file_list = [
+    ]
+    assert_equal [
+    ], mime_types(extension_hash, file_list)
     
-    assert_equal ["text/html"], mime_types(file_list, extension_hash)
+  end
+
+  def test02_one_file_list_with_one_known_mime_type
+
+    extension_hash = { 
+    "html" => "text/html" 
+    }
+    file_list = [
+    "file.html"
+    ]
+    assert_equal [
+    "text/html"
+    ], mime_types(extension_hash, file_list)
+    
   end  
 
-  def test03
-	  
-    extension_hash = { "html" => "text/html" }
-    file_list = ["file.html"]
+  def test03_two_files_list_with_two_known_mime_types
+
+    extension_hash = { 
+    "html" => "text/html", 
+    "gif" => "image/gif" 
+    }
+    file_list = [
+    "file.html", 
+    "file.gif"
+    ]
+    assert_equal [
+    "text/html", 
+    "image/gif"
+    ], mime_types(extension_hash, file_list)
     
-    assert_equal ["text/html"], mime_types(file_list, extension_hash)
-    
-  end  
-  
-  def test04
-	  
-    extension_hash = { "html" => "text/html" }
-    file_list = ["file.html", "file.gif"]
-    
-    assert_equal ["text/html", "UNKNOWN"], mime_types(file_list, extension_hash)
+  end
+
+  def test04_two_files_list_with_one_UNKNOWN_mime_type
+
+    extension_hash = { 
+    "html" => "text/html" 
+    }
+    file_list = [
+    "file.html", 
+    "file.gif"
+    ]
+    assert_equal [
+    "text/html", 
+    "UNKNOWN"
+    ], mime_types(extension_hash, file_list)
     
   end
   
-  def test05
-	  
-    extension_hash = { "html" => "text/html", "gif" => "image/gif" }
-    file_list = ["file.html", "file.gif"]
-    
-    assert_equal ["text/html", "image/gif"], mime_types(file_list, extension_hash)
-    
-  end  
-	
-  def test11
-
-    extension_hash = {"html" =>  "text/html", "png" => "image/png", "gif" => "image/gif" }
-    file_list = ["animated.gif", "portrait.png", "index.html"]
-    
-    assert_equal  ["image/gif", "image/png", "text/html"], mime_types(file_list, extension_hash)
-
-  end
-  
-  def test12
-    
-    extension_hash = {"txt" => "text/plain", "xml" =>  "text/xml", "flv" => "video/x-flv" }
-    file_list = ["image.png", "animated.gif", "script.js", "source.cpp"]   
-    
-    assert_equal  ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"], mime_types(file_list, extension_hash)
-    
-  end
-  
-  def test13
-
-    extension_hash = { "wav" => "audio/x-wav", "mp3" => "audio/mpeg", "pdf" => "application/pdf" }
-    file_list = ["a", "a.wav", "b.wav.tmp", "test.vmp3", "pdf", "mp3", "report..pdf", "defaultwav", ".mp3.", "final."]   
-    
-    assert_equal  ["UNKNOWN", "audio/x-wav", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "application/pdf", "UNKNOWN", "UNKNOWN", "UNKNOWN"], mime_types(file_list, extension_hash)
+  def test05_file_list_with_particular_extensions
+    extension_hash = { 
+    "wav" => "audio/x-wav", 
+    "mp3" => "audio/mpeg", 
+    "pdf" => "application/pdf" 
+    }
+    file_list = [
+    "a", 
+    "a.wav", 
+    "b.wav.tmp", 
+    "test.vmp3", 
+    "pdf", "mp3", 
+    "report..pdf", 
+    "defaultwav", 
+    ".mp3.", "final."
+    ]
+    assert_equal  [
+    "UNKNOWN", 
+    "audio/x-wav", 
+    "UNKNOWN", 
+    "UNKNOWN", 
+    "UNKNOWN", 
+    "UNKNOWN", 
+    "application/pdf", 
+    "UNKNOWN", 
+    "UNKNOWN", 
+    "UNKNOWN"
+    ], mime_types(extension_hash, file_list)
 
   end
 
-  def test14
+  def test06_file_list_with_upper_or_lower_case_extensions
 
-    #~ extension_hash = { "png" => "image/png", "TIFF" => "image/TIFF", "css" => "text/css", "TXT" => "text/plain"}
-    extension_hash = { "png" => "image/png", "tiff" => "image/TIFF", "css" => "text/css", "txt" => "text/plain"}
-    file_list = ["example.TXT", "referecnce.txt", "strangename.tiff", "resolv.CSS", "matrix.TiFF", "lanDsCape.Png", "extract.cSs"]   
+    extension_hash = { 
+    "png" => "image/png", 
+    "tiff" => "image/TIFF", 
+    "css" => "text/css", 
+    "txt" => "text/plain"
+    }
+    file_list = [
+    "example.TXT", 
+    "referecnce.txt", 
+    "strangename.tiff", 
+    "resolv.CSS", 
+    "matrix.TiFF", 
+    "lanDsCape.Png", 
+    "extract.cSs"
+    ]
+    assert_equal  [
+    "text/plain", 
+    "text/plain", 
+    "image/TIFF", 
+    "text/css", 
+    "image/TIFF", 
+    "image/png", 
+    "text/css"
+    ], mime_types(extension_hash, file_list)
     
-    assert_equal  ["text/plain", "text/plain", "image/TIFF", "text/css", "image/TIFF", "image/png", "text/css"], mime_types(file_list, extension_hash)
-
   end
-  
-  def test15
-
-    extension_hash = { }
-    file_list = []   
-    
-    assert_equal  [], mime_types(file_list, extension_hash)
-
-  end 
   
 end
--- mimetypes/mimetype.java.rb
+++ mimetypes/mimetype.java.rb
@@ -2,59 +2,42 @@
 #~ import java.io.*;
 #~ import java.math.*;
 
-#~ class Solution {
 class Solution
 
-	#~ public static void main(String args[]) {
-		
-		# read two first lines
-		#~ Scanner in = new Scanner(System.in);
-		#~ int n = Integer.valueOf(in.nextLine().trim());
-		#~ int q = Integer.valueOf(in.nextLine().trim());
-		#~ n = Math.max(0, Math.min(n, 10000));
-		#~ q = Math.max(0, Math.min(q, 10000));
-		
-		# put n next lines in a hash
-		#~ Map<String, String> table = new HashMap<String, String>();
-		#~ for (int i = 0; i < n; i++) {
-		    #~ String[] assoc = in.nextLine().split(" ");
-		    #~ table.put(assoc[0].toUpperCase(), assoc[1]);
-		#~ }
-		
-		table = {}
-		
-		q = 0
-		fichiers = ['..']
+	def Solution.main(table, fichiers)
 		
 		# map each lines
 		#~ for (int i = 0; i < q; i++) {
-		for i in 0..q do
+		for i in 0..fichiers.size-1 do
 		    #~ String nomFichier = in.nextLine().trim();
 		    nomFichier = fichiers[i]
 		    #~ if (nomFichier.lastIndexOf(".") < 0)
-		    if nomFichier.rindex(".") < 0
+		    if (nomFichier.rindex(".") < 0)
 			#~ System.out.println("UNKNOWN");
-			puts "UNKNOWN"
+			$stdout.puts "UNKNOWN"
 		    #~ else {
 		    else
 			#~ String ext = nomFichier.substring(nomFichier.lastIndexOf(".") + 1).toUpperCase();
-			ext = nomFichier[nomFichier.rindex(".")..-1].upcase;
+			ext = nomFichier[nomFichier.rindex(".")+1, nomFichier.size-1].upcase;
 			#~ if (table.containsKey(ext))
 			if table.has_key?(ext)
 			    #~ System.out.println(table.get(ext));
-			    puts table[ext]
+			    $stdout.puts table[ext]
 			else
 			    #~ System.out.println("UNKNOWN");
-			    puts "UNKNOWN"
+			    $stdout.puts "UNKNOWN"
 			#~ }
 			end
 		    end
+		    i = i + 1
 		#~ }
 		end
-		
+	end	
 	#~ }
 #~ }
 end
+
+Solution.main({}, ['..'])
 
 #~ import java.util.*;
 #~ import java.io.*;
